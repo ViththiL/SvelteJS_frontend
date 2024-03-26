2b79f97e38c99d2cb6dd49dba64582a714d792e7
ViththiL <viththisha68@gmail.com>
2024-03-26 15:56:41 +0530

    svelte binary modifications

--- scripts/package.cjs
+++ scripts/package.cjs
@@ -1,10 +1,4 @@
 const { execSync } = require('child_process');
 
-// Path to your source directory (modify as needed)
-const srcPath = './src';
-
-// Copy the source directory to the build directory
-execSync(`cp -r ${srcPath} ./dist`);
-
 // Package the app using the existing pkg command
-execSync('npm run package');
+execSync('npm run build');
